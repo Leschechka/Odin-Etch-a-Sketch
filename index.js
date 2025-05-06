56ea5fe3454f2586de74be9916bd8a64e5818bb3
Alex <suslo2000@list.ru>
2025-05-06 17:29:01 +0300

    Created function which builds flex rows in .screen div, and flex items in those rows with shrink and grow values

---
+++ index.js
@@ -0,0 +1,18 @@
+function createPixels(number) {
+  for (let i = 0; i < number; i++) {
+    const div = document.createElement('div')
+    div.classList.add('row')
+		screen.appendChild(div)
+
+    for (let j = 0; j < number; j++) {
+      const subdiv = document.createElement('div')
+      subdiv.classList.add('pixel')
+      div.appendChild(subdiv)
+    }
+
+  }
+}
+
+const screen = document.getElementById('screen')
+
+createPixels(100)
