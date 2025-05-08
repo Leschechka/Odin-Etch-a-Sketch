36d627f461b73aa9bd7edb30465d4433df43d2c7
Alex <suslo2000@list.ru>
2025-05-09 02:40:18 +0300

    adding event listener to 'screen' div to get possibility of painting its child elements using mouseover event

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ function createPixels(number) {
   for (let i = 0; i < number; i++) {
     const div = document.createElement('div')
     div.classList.add('row')
-		screen.appendChild(div)
+    screen.appendChild(div)
 
     for (let j = 0; j < number; j++) {
       const subdiv = document.createElement('div')
       subdiv.classList.add('pixel')
       div.appendChild(subdiv)
     }
-
   }
 }
 
+
 const screen = document.getElementById('screen')
 
-createPixels(100)
+createPixels(16)
+
+screen.addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'black');
